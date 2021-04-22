c1210a375a92f52ae3caf7bdf34b38ae24597199
PuruBehl <46553962+PuruBehl@users.noreply.github.com>
2021-04-22 14:42:56 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,9 +3,8 @@ echo "\
 [general]\n\
 email = \"your@domain.com\"\n\
 " > ~/.streamlit/credentials.toml
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false
 " > ~/.streamlit/config.toml
