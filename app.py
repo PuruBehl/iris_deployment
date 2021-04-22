b7c348d1a7a9dfa74c8ef1d7596e4bc06107aa50
PuruBehl <46553962+PuruBehl@users.noreply.github.com>
2021-04-22 10:11:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,51 +4,52 @@ import streamlit as st
 from sklearn.tree import DecisionTreeClassifier
 
 
+def run() :
+	# Adding title to the project
+	st.title("Iris species Detection")
 
-# Adding title to the project
-st.title("Iris species Detection")
+	st.markdown("Sepal length :")
 
-st.markdown("Sepal length :")
-
-sl=st.text_input('1')
-
-
-st.markdown("Sepal Width :")
-
-sw=st.text_input("2")
+	sl=st.text_input('1')
 
 
-st.markdown("Petal Length :")
+	st.markdown("Sepal Width :")
 
-pl=st.text_input("3")
-
-
-st.markdown("Petal Width :")
-
-pw=st.text_input("4")
+	sw=st.text_input("2")
 
 
-path='model/Tree_model_iris.pkl'
+	st.markdown("Petal Length :")
 
-# Loading the model (Decision Tree Model)
-
-
-flowers=['setose','versicolor','virginica']
-
-if st.button("Get Prediction"):
-	model=pickle.load(open(path,'rb'))
-
-	pw=float(pw)
-
-	pl=float(pl)
-	sw=float(sw)
-	sl=float(sl)
+	pl=st.text_input("3")
 
 
+	st.markdown("Petal Width :")
 
-	prediction=model.predict([[sl,sw,pl,pw]])
-	answer='The Type of the iris species is :  '+flowers[prediction[0]][0].upper()+flowers[prediction[0]][1:]
-	st.info(answer)
+	pw=st.text_input("4")
 
 
+	path='model/Tree_model_iris.pkl'
+
+	# Loading the model (Decision Tree Model)
+
+
+	flowers=['setose','versicolor','virginica']
+
+	if st.button("Get Prediction"):
+		model=pickle.load(open(path,'rb'))
+
+		pw=float(pw)
+
+		pl=float(pl)
+		sw=float(sw)
+		sl=float(sl)
+
+
+
+		prediction=model.predict([[sl,sw,pl,pw]])
+		answer='The Type of the iris species is :  '+flowers[prediction[0]][0].upper()+flowers[prediction[0]][1:]
+		st.info(answer)
+
+if __name__ == "__main__":
+    run()
 
